0760d92b4a4a14328dfc80ece009331a90f69f02
waseem-qasim <waseemqasim@gmail.com>
2022-05-20 16:42:46 +0500

    first assignment javascript

---
+++ index.js
@@ -0,0 +1,40 @@
+// task 1
+alert ("Error! Please enter avalid password");
+
+// task 2
+alert ("Welcome to JS Land...\nHappy Coding!");
+
+// task 3
+alert ("Welcome to JS Land...");
+
+// task 4
+var checkbox = ("Happy Coding! \nPrevent this page creating additional dialogs ")
+alert (checkbox)
+
+// task 5
+alert ("Hello... ! can run JS through my web browser console");
+
+// task 6
+var name = ("Jhone Doe")
+alert (name)
+var age = ("15 years old")
+alert (age)
+var certified = ("Certified Mobile Apllication Developmen")
+alert (certified)
+
+// task 7
+alert ("PIZZA \nPIZZ \nPIZ \nPI \nP");
+
+// task 8
+var email = ("My Email Id is example@exampel.com")
+alert (email)
+
+// task 9
+var book = ("Iam trying to learn from the Book A smarter \nway to learn JavaScript")
+alert (book)
+
+// task 10
+alert ("▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬");
+
+// task 11
+document.write("Yah! I can write HTML content through JavaScript")
